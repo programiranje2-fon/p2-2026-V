9b9bb59d51e38fa84b44fd1de2125ac1dac4a5d1
jeljov <jeljov@gmail.com>
2026-03-19 11:24:19 +0100

    Lab1 tasks added

---
+++ lab1/lab1_start.py
@@ -0,0 +1,39 @@
+# VEZBE 1
+
+
+if __name__ == '__main__':
+    pass
+
+    # Zadatak 1
+    # odd_or_even()
+
+
+    # Zadatak 2
+    # print(factorial(5))
+
+
+    # Zadatak 3
+    # numbers  = [31, 72, 13, 41, 5, 16, 87, 98, 9]
+    # print(f"3rd lowest among numbers: {numbers}:")
+    # print(nth_lowest(numbers,3))
+
+    # letters = ['f', 'r', 't', 'a', 'b', 'y', 'j', 'd', 'c']
+    # print(f"6th lowest among letters: {letters}:")
+    # print(nth_lowest(letters, 6))
+
+    # s = "today"
+    # print(f"2nd lowest in string: '{s}':")
+    # print(nth_lowest(s, 2))
+
+
+    # Zadatak 4
+    # print(list_stats([3.4, 5.6, -4.2, -5.6, 9, 1.2, 11.3, -23.45, -81]))
+
+
+    # Zadatak 5
+    # list_operations([1, 1, 2, 3, 5, 8, 13, 5, 21, 34, 55, 89], 20)
+
+
+    # Zadatak 6
+    # guessing_game()
+
