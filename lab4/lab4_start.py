5893ee85589b84d430876bd7e3c69d07f8cb57f6
jeljov <jeljov@gmail.com>
2026-04-16 11:16:54 +0200

    Lab4 tasks added

---
+++ lab4/lab4_start.py
@@ -0,0 +1,54 @@
+"""
+Vezbe, dvocas 4
+"""
+
+
+
+if __name__ == "__main__":
+
+    pass
+
+    # ZADATAK 1
+    # print(compute_product(1,-4,13,2))
+    # print(compute_product(1, -4, 13, 2, squared=True))
+    # print()
+
+    # # Calling the compute_product function with a list
+    # num_list = [2, 7, -11, 9, 24, -3]
+    # # This is NOT a way to make the call:
+    # print("Calling the function by passing a list as the argument")
+    # print(compute_product(num_list))
+    # print()
+
+    # # instead, this is how it should be done (the * operator is 'unpacking' the list):
+    # print("Calling the function by passing an UNPACKED list as the argument")
+    # print(compute_product(*num_list))
+
+
+    # ZADATAK 2
+    # str_list = ['yellowy', 'Bob', 'lovely', 'Yesterday', 'too']
+    # print(select_strings(*str_list))
+
+
+    # ZADATAK 3
+    # orders = [("34587", "Learning Python, Mark Lutz", 4, 40.95),
+    #           ("98762", "Programming Python, Mark Lutz", 5, 56.80),
+    #           ("77226", "Head First Python, Paul Barry", 3, 32.95),
+    #           ("88112", "Einführung in Python3, Bernd Klein", 3, 24.99)]
+    #
+    # print(process_product_orders(orders))
+    # print()
+    # print("The same orders with discount of 10%")
+    # print(process_product_orders(orders, discount=10))
+
+
+    # ZADATAK 4
+    # print(compute_sum_loop(10000))
+    # print()
+    # print(compute_sum_lc(10000))
+    # print()
+    # print(compute_sum_mr(10000))
+
+
+    # ZADATAK 5
+    # print(sum_of_sums(1,3,5,7,9,11,13, n=7))
